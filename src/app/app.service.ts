dd6b59f8b2727223742cefde406778d816f87e1c
raul.zepeda@gistsdev.com <raul.zepeda@gistsdev.com>
2025-07-07 17:47:53 -0600

    Multiple empresa version prod

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,14 +1,11 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable(
-    {
-        providedIn: 'root'
-    }
-)
-
+@Injectable({
+  providedIn: 'root',
+})
 export class ApiService {
-    ApiURL = 'http://localhost:3000';
-    //ApiURL = 'https://dte-backend-e894411e55f9.herokuapp.com'; //PROD
-    constructor(private http: HttpClient) { }
+  //ApiURL = 'http://localhost:3000';
+  ApiURL = 'https://dte-backend-e894411e55f9.herokuapp.com'; //PROD
+  constructor(private http: HttpClient) {}
 }
